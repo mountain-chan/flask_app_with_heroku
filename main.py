58656345f29bdd7db3d8ed9366fb5e590fe433e5
Ly Van Chan <chan.ly@boot.ai>
2020-06-30 16:08:30 +0700

    up code 2

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, Config
 
 app = Flask(__name__)
+
+
+class ProdConfig(Config):
+    ENV = 'production'
+    DEBUG = False
+    DEBUG_TB_ENABLED = False
+    HOST = '0.0.0.0'
+    TEMPLATES_AUTO_RELOAD = False
+
+
+app.config.from_object(ProdConfig)
 
 
 @app.route('/')
